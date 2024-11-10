7124026a2b6e337af6120904337b5490cfc5e419
cab-1729 <60106638+cab-1729@users.noreply.github.com>
2024-11-10 07:09:15 +0530

    Added rudimentary text wrap around image

--- capture.lisp
+++ capture.lisp
@@ -1,4 +1,18 @@
-(defconstant THREAD "https://www.4chan.org/g/thread/100883730")
-(defconstant INCLUDE ">>101125290
-			 >>101125346>>101126536
+(defconstant THREAD "https://www.4chan.org/g/thread/103105320")
+(defconstant INCLUDE ">>103105329
+			 >>103105747
+			 >>103106179
+			 >>103106255
+			 >>103106841
+			 >>103106520
+			 >>103109514
+			 >>103110941
 			 ")
+;image
+;webm
+;gif
+;imageless
+;code
+;image + code
+;image + small code
+;large code
--- main.lisp
+++ main.lisp
@@ -8,6 +8,8 @@
 ;;TODO: make loading paths absolute
 (load "parse.lisp")
 (ql:quickload "lisp-magick-wand")
+(ql:quickload "cl-pango")
+(ql:quickload "cl-cairo2")
 (ql:quickload "dexador")
 (ql:quickload "lquery")
 (ql:quickload "local-time")
@@ -29,12 +31,199 @@
 		)))
 	)
 )
+(defun post-text (gap_x gap_y post_x post_y background_color pango-text)
+	(let* (
+			(image (
+				magick:new-magick-wand
+			))
+			(surface (
+				cairo:create-image-surface :argb32 post_x post_y
+			))
+			(cr (
+				cairo:create-context surface
+			))
+			(context (
+				pango:pango_font_map_create_context (
+					pango:pango_cairo_font_map_new
+				)
+			))
+			(layout (
+				pango:pango_layout_new context
+			))
+			(last-rendered-index 0)
+		)
+		(cairo:set-source-rgba (
+				/ (
+					parse-integer (
+						subseq background 1 3
+					) :radix 16
+				) 255.0
+			) (
+				/ (
+					parse-integer (
+						subseq background 3 5
+					) :radix 16
+				) 255.0
+			) (
+				/ (
+					parse-integer (
+						subseq background 5 7
+					) :radix 16
+				) 255.0
+			)
+			1.0 cr)
+		(cairo:paint cr)
+		(cairo:set-source-rgba (
+				/ (
+					parse-integer (
+						subseq TEXT 1 3
+					) :radix 16
+				) 255.0
+			) (
+				/ (
+					parse-integer (
+						subseq TEXT 3 5
+					) :radix 16
+				) 255.0
+			) (
+				/ (
+					parse-integer (
+						subseq TEXT 5 7
+					) :radix 16
+				) 255.0
+			)
+			1.0 cr)
+		(cairo:move-to gap_x 0 cr)
+		(pango:pango_layout_set_font_description layout (
+			pango:pango_font_description_from_string "Arial 10"
+		))
+		(pango:pango_layout_set_markup layout pango-text -1)
+		(pango:pango_layout_set_width layout (
+			* pango:PANGO_SCALE (
+				- POST_X gap_x
+		)))
+		(pango:pango_layout_set_height layout (
+			* pango:PANGO_SCALE gap_y
+		))
+		(pango:pango_cairo_show_layout (
+			slot-value cr 'cairo::pointer
+		) layout)
+		(setq last-rendered-index (
+			- (
+				length (
+					pango:pango_layout_get_text layout
+				)) 2
+		))
+		(cffi:with-foreign-object (;find wrap cursor
+				rect 'pango:PangoRectangle
+			)
+			(pango:pango_layout_index_to_pos layout last-rendered-index rect)
+			(if (
+				= 0 ( ;wrap needed
+						cffi:foreign-slot-value rect 'pango:PangoRectangle 'pango::height
+					))
+				(let (
+					(cursor 0)
+					(open-tag NIL)
+					(close-tag NIL)
+					(in-escape NIL)
+					(tags NIL)
+					(bottom-text NIL)
+				)
+				(setq bottom-text (
+					subseq pango-text 
+						(-
+							(loop for i from 0 below (length pango-text);find wrap point
+							for char = (char pango-text i) do;ChatGPT
+							(pango:pango_layout_index_to_pos layout cursor rect)
+							(if (
+								= 0 (
+									cffi:foreign-slot-value rect 'pango:PangoRectangle 'pango::height
+								)
+							) (return i))
+							(case char
+								(#\< (
+									if (
+										eq (
+											char pango-text (
+												+ 1 i
+											)) #\/
+										)(;closing tag
+											progn 
+											(setq close-tag T)
+											(setq tags (
+												cdr tags
+											))
+										)(
+											setq open-tag i;mark index for string extraction
+										)))
+								 (#\> (
+									if open-tag (
+										progn (
+											setq tags (cons (
+												subseq pango-text open-tag (
+													+ i 1
+												))
+											   tags ))
+										(setq open-tag NIL))
+										(setq close-tag NIL)
+									   ))
+								 (#\& (
+									   setq in-escape T
+									   )(
+									   setq cursor (+ cursor 1)
+									   ))
+								 (#\; (
+									   setq in-escape NIL
+									   ))
+								 (otherwise (
+									if (
+										not (
+											or open-tag close-tag in-escape
+										))(setq cursor (
+											+ cursor 1
+										)))))
+							) 1)
+						))
+				(cairo:move-to 0 gap_y cr)
+				(pango:pango_layout_set_width layout (
+					* pango:PANGO_SCALE gap_x
+				))
+				(pango:pango_layout_set_height layout (
+					* pango:PANGO_SCALE (
+						- post_y gap_y
+					)))
+				(pango:pango_layout_set_markup layout (
+					str:concat (
+						format nil "~{~A~}" (
+							reverse tags
+						)
+					) bottom-text
+				) -1)
+				(pango:pango_cairo_show_layout (
+					slot-value cr 'cairo::pointer
+				) layout
+				)
+				)))
+			(magick:constitute-image image post_x post_y "BGRA" ':char (
+				sb-sys:vector-sap (
+					make-array (
+						* 4 post_x post_y; total pixel data
+					) :element-type '(unsigned-byte 8) :initial-contents (
+						coerce (
+							cairo:image-surface-get-data surface
+						) 'list
+					))))
+			(cairo:destroy surface)
+			(return-from post-text image)
+	)
+)
 ;;TODO read thread from capture.lisp
 ;;NOTE: not using 4chan API because of deleted threads/posts
 (defconstant PAGE (
 	lquery:$ (
 		initialize (
-			dex:get "https://desuarchive.org/g/thread/102165191"
+			dex:get "https://desuarchive.org/g/thread/103105320"
 ))))
 (defvar *image* (
 	magick:new-magick-wand
@@ -42,23 +231,15 @@
 (defvar *pixel-wand* (
 	magick:new-pixel-wand
 ))
-(magick:set-size *image* 2000 1000);;TODO: Calculate dimensions
+(magick:set-size *image* 2000 2000);;TODO: Calculate dimensions
 (magick:read-image *image* (
 	concatenate 'string "xc:" BACKGROUND
 ))
 ;;TODO: figure out text wrapping
 ;;TODO: figure out text indentation around image
-(let* (
+(let (
 	(op-id (
 		lquery:$1 PAGE "header > div[class=post_data] > a[data-function=highlight]" (attr "data-post")
-	))
-	(op-image-element (
-		lquery:$1 PAGE "a[class=thread_image_link]"
-	))
-	(op-image-width (
-		parse-integer(
-			lquery:$1 op-image-element "img" (attr "width")
-		)
 	))
 )
 	(if (
@@ -66,120 +247,80 @@
 			member op-id *posts* :test 'string-equal
 		)
 	)
-		(progn
-			(let (
-				(op-image (magick:new-magick-wand))
-			)
-				(magick:read-image op-image (
-					lquery:$1 op-image-element (attr "href")
-				))
-				(magick:resize-image op-image op-image-width (
+		(let* (
+			(op-image-element (
+				lquery:$1 PAGE "a[class=thread_image_link]"
+			))
+			(op-image-width (
+				parse-integer(
+					lquery:$1 op-image-element "img" (attr "width")
+				)))
+			(op-image-x (
+				+ 20 op-image-width
+			))
+			(op-image-y (
+				+ 20 (
 					parse-integer(
 						lquery:$1 op-image-element "img" (attr "height")
-					)
-				) 4;;CubicGaussian 
+					))))
+			(op-post (
+				magick:new-magick-wand
+			))
+			(op-image (
+				magick:new-magick-wand
+			))
+			(op-post-info (
+				magick:new-magick-wand
+			))
+		)
+			(magick:set-size op-post 2000 2000);;TODO: Calculate dimensions
+			(magick:read-image op-post (
+				concatenate 'string "xc:" BACKGROUND
+			))
+			(magick:read-image op-image (
+				lquery:$1 op-image-element (attr "href")
+			))
+			(magick:resize-image op-image op-image-width (
+				parse-integer(
+					lquery:$1 op-image-element "img" (attr "height")
 				)
-				(magick:composite-image *image* op-image
-					54 ;;OverCompositeImage https://github.com/ImageMagick/ImageMagick/blob/5fcf6ae2a93af8771b6a407eb8e14a27ced54bc2/MagickCore/composite.h#L81
-				T 0 0)
+			) 4;;CubicGaussian 
 			)
-			(let (
-				(op-post (magick:new-magick-wand))
-			)
-				(magick:set-font op-post "fonts/ARIAL.TTF")
-				(magick:set-pointsize op-post 10.0)
-				(magick:pixel-set-color *pixel-wand* BACKGROUND)
-				(magick:set-background-color op-post *pixel-wand*)
-				(magick:read-image op-post (
-					format nil "pango:<span background=\"~a\" weight=\"bold\"><span foreground=\"~a\">~a</span> <span foreground=\"~a\">~a</span> <span foreground=\"~a\">~a No.~a  <span foreground=\"~a\">~a</span></span>
-
-~a</span>" BACKGROUND TITLE (
-						str:replace-all "&" "&#38;" (
-							lquery:$1 page "h2[class=post_title]" (text) ;; thread title
-					)) NAME (
-						lquery:$1 page "header > div[class=post_data] > span[class=post_poster_data] > span[class=post_author]" (text) ;;op name
-					)
-					TEXT (
-						local-time:format-timestring nil (
-							local-time:parse-timestring (
-								lquery:$1 PAGE "header > div[class=post_data] > span[class=time_wrap] > time" (attr "datetime")
-							)
-						) :format '(
-							(:month 2) "/" (:day 2) "/" :short-year "(" :short-weekday ")" (:hour 2) ":" (:min 2) ":" (:sec 2)
+			(magick:set-font op-post-info "fonts/ARIAL.TTF")
+			(magick:set-pointsize op-post-info 10.0)
+			(magick:pixel-set-color *pixel-wand* BACKGROUND)
+			(magick:set-background-color op-post-info *pixel-wand*)
+			(magick:read-image op-post-info (
+				format nil "pango:<span background=\"~a\" weight=\"bold\"><span foreground=\"~a\">~a</span> <span foreground=\"~a\">~a</span> <span foreground=\"~a\">~a No.~a  <span foreground=\"~a\">~a</span></span></span>" BACKGROUND TITLE (
+					str:replace-all "&" "&#38;" (
+						lquery:$1 page "h2[class=post_title]" (text) ;; thread title
+				)) NAME (
+					lquery:$1 page "header > div[class=post_data] > span[class=post_poster_data] > span[class=post_author]" (text) ;;op name
+				)
+				TEXT (
+					local-time:format-timestring nil (
+						local-time:parse-timestring (
+							lquery:$1 PAGE "header > div[class=post_data] > span[class=time_wrap] > time" (attr "datetime")
 						)
-					) op-id QUOTEDBY (
-						lquery:$1 PAGE "div[class=backlink_list] > span[class=post_backlink]" (text)
-					) (
-						pangofy(
-							lquery:$1 PAGE "div[class=text]"
-				))))
-				(magick:composite-image *image* op-post 54 T (
-					+ op-image-width 20
-				) 10)
-				(setq *posts* (
-					remove op-id *posts* :test 'string-equal
-				))
-			)
-		)
-	)
-)
-(magick:pixel-set-color *pixel-wand* POSTBACKGROUND)
-(loop for post-id in *posts* do (
-	let* (
-		(post-selector (
-			format nil "article[id=~a]" post-id
-		))
-		(post-element (
-			lquery:$1 PAGE post-selector
-		))
-		(post-header (
-			magick:new-magick-wand
-		))
-		(post-text (
-			magick:new-magick-wand
-		))
-		(post-image-element(
-			lquery:$1 post-element "div[class=post_wrapper]  > div[class=thread_image_box]"
-		))
-	)
-	(magick:set-background-color post-header *pixel-wand*)
-	(magick:set-font post-header "fonts/ARIAL.TTF")
-	(magick:set-pointsize post-header 10.0)
-	(magick:set-background-color post-text *pixel-wand*)
-	(magick:set-font post-text "fonts/ARIAL.TTF")
-	(magick:set-pointsize post-text 10.0)
-	(magick:read-image post-header (
-		format nil "pango:<span background=\"~a\" weight=\"bold\"><span foreground=\"~a\">~a</span> <span foreground=\"~a\">~a No.~a  <span foreground=\"~a\">~a</span></span></span>" POSTBACKGROUND NAME (
-			lquery:$1 post-element "div[class=post_wrapper] > header > div[class=post_data] > span[class=post_poster_data] > span[class=post_author]" (text) ;;poster name
-		)
-		TEXT (
-			local-time:format-timestring nil ( ;;post time
-				local-time:parse-timestring (
-					lquery:$1 post-element "div[class=post_wrapper] > header > div[class=post_data] > span[class=time_wrap] > time" (attr "datetime")
+					) :format '(
+						(:month 2) "/" (:day 2) "/" :short-year "(" :short-weekday ")" (:hour 2) ":" (:min 2) ":" (:sec 2)
+					)
+				) op-id QUOTEDBY (
+					lquery:$1 PAGE "div[class=backlink_list] > span[class=post_backlink]" (text)
+				)))
+			(magick:composite-image op-post op-post-info 54 T op-image-x 0)
+			(setq *posts* (
+				remove op-id *posts* :test 'string-equal
+			))
+			(magick:composite-image op-post (
+				post-text op-image-x op-image-y 2000 1000 BACKGROUND (
+					pangofy (
+						lquery:$1 PAGE "div[class=text]"
+					)
 				)
-			) :format '(
-				(:month 2) "/" (:day 2) "/" :short-year "(" :short-weekday ")" (:hour 2) ":" (:min 2) ":" (:sec 2)
-			)
-		) post-id QUOTEDBY (
-			lquery:$1 post-element "div[class=backlink_list] > span[class=post_backlink]" (text)
-		)
-	))
-	(lquery:$ post-element "div[class=text] br" (replace-with "
-"))
-;;TODO: add "(OP)" when OP is quoted
-	(lquery:$ post-element "div[class=text] > span[class=greentext] > a[data-backlink=true]" (unwrap) (wrap-inner (format nil "<span foreground=\"~a\">" QUOTED)) (children) (unwrap)) ;; highlight quotes
-	(lquery:$ post-element "div[class=text] > a[target=_blank]" (wrap-inner (format nil "<span foreground=\"~a\">" LINK)) (children) (unwrap));; color links
-;;TODO:greentext
-;;TODO:links
-;;TODO:code
-;;TODO:/pol/ flags and ids
-;;TODO:trips
-	(magick:read-image post-text (
-		format nil "pango:<span background=\"~a\" weight=\"bold\">~a</span>" POSTBACKGROUND (
-			str:substring 19 -7 (
-				lquery:$1 post-element "div[class=text]" (serialize)
-			)
-		)
-	))
-))
-(magick:write-image *image* "test.png")
+			) 54 T 0 23)
+			(magick:composite-image op-post op-image
+				54 ;;OverCompositeImage https://github.com/ImageMagick/ImageMagick/blob/5fcf6ae2a93af8771b6a407eb8e14a27ced54bc2/MagickCore/composite.h#L81
+			T 0 0)
+			(magick:write-image op-post "test.png") ;; testing
+		)))
